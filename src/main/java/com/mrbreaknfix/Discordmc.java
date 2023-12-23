ba085a1c54cd26885d25d7f94343cb46febce5f5
MrBreakNFix <73456238+MrBreakNFix@users.noreply.github.com>
2023-12-23 11:51:22 -0800

    Add files via upload

--- src/main/java/com/mrbreaknfix/Discordmc.java
+++ src/main/java/com/mrbreaknfix/Discordmc.java
@@ -1,7 +1,13 @@
 package com.mrbreaknfix;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +16,7 @@ public class Discordmc implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("discordmc");
+	private static final MinecraftClient mc = MinecraftClient.getInstance();
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +25,23 @@ public class Discordmc implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		JDABuilder jdaBuilder = JDABuilder.createDefault("")
+				.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES)
+				.addEventListeners(new Discordmc());
+		JDA jda = jdaBuilder.build();
+	}
+	public void onMessageReceived(MessageReceivedEvent event) {
+		System.out.println("received a message from " + event.getAuthor().getName() + ": " + event.getMessage().getContentRaw());
+
+		if (event.getChannel().getName().equals("testchannel")) {
+			if (!(mc.player == null))
+				mc.player.sendMessage(Text.of("§9<" + event.getAuthor().getName() + ">§b " + event.getMessage().getContentRaw()));
+
+
+			if (event.getMessage().getContentRaw().equals("ping")) {
+				event.getChannel().sendMessage("pong").queue();
+			}
+		}
 	}
 }
