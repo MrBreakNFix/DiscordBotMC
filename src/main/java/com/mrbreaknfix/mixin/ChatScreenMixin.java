5582e43a889af22917ed752eaa0dc8134343642f
MrBreakNFix <73456238+MrBreakNFix@users.noreply.github.com>
2023-12-28 14:27:38 -0800

    config
    tried to do gson config

--- src/main/java/com/mrbreaknfix/mixin/ChatScreenMixin.java
+++ src/main/java/com/mrbreaknfix/mixin/ChatScreenMixin.java
@@ -16,56 +16,55 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-
 @Mixin(ChatScreen.class)
-
 public class ChatScreenMixin extends Screen {
-	protected ChatScreenMixin(Text title) {
-		super(title);
-	}
+    protected ChatScreenMixin(Text title) {
+        super(title);
+    }
 
-	@Inject(at = @At("HEAD"), method = "sendMessage", cancellable = true)
-	public void sendMessage(String chatText, boolean addToHistory, CallbackInfoReturnable<Boolean> cir) {
-		MinecraftClient mc = MinecraftClient.getInstance();
-		if(chatText.toLowerCase().startsWith(".d") || (chatText.toLowerCase().startsWith(",d"))) {
-			Discordmc.sendDiscordMessage(chatText.substring(3), Discordmc.currentGuild, Discordmc.currentChat);
+    @Inject(at = @At("HEAD"), method = "sendMessage", cancellable = true)
+    public void sendMessage(String chatText, boolean addToHistory, CallbackInfoReturnable<Boolean> cir) {
+        MinecraftClient mc = MinecraftClient.getInstance();
+        if(chatText.toLowerCase().startsWith(".d") || (chatText.toLowerCase().startsWith(",d"))) {
+            Discordmc.sendDiscordMessage(chatText.substring(3), Discordmc.config.getCurrentGuild(), Discordmc.config.getCurrentChat());
 
-			mc.setScreen(null);
-			cir.setReturnValue(false);
-		}
+            mc.setScreen(null);
+            cir.setReturnValue(false);
+        }
 
-		if (Discordmc.discordChatEnabled && !chatText.startsWith("/") && !chatText.startsWith(".") && !chatText.startsWith(",")) {
+        if (Discordmc.config.isDiscordChatEnabled() && !chatText.startsWith("/") && !chatText.startsWith(".") && !chatText.startsWith(",")) {
 
-			Discordmc.sendDiscordMessage(chatText, Discordmc.currentGuild, Discordmc.currentChat);
+            Discordmc.sendDiscordMessage(chatText, Discordmc.config.getCurrentGuild(), Discordmc.config.getCurrentChat());
 
-			mc.setScreen(null);
-			cir.setReturnValue(false);
-		}
-	}
-	@Inject(at = @At("TAIL"), method = "init")
-	public void init(CallbackInfo ci) {
-		MinecraftClient mc = MinecraftClient.getInstance();
+            mc.setScreen(null);
+            cir.setReturnValue(false);
+        }
+    }
 
-		MutableText channel = Text.literal(Discordmc.currentChat + "   ").formatted(Formatting.WHITE);
-		MutableText guild = Text.literal(Discordmc.currentGuild).formatted(Formatting.WHITE);
+    @Inject(at = @At("TAIL"), method = "init")
+    public void init(CallbackInfo ci) {
+        MinecraftClient mc = MinecraftClient.getInstance();
 
-		MutableText chatDestination = Text.literal("Chatting to: ").formatted(Formatting.GRAY).append(guild).append(Text.literal(", in #").formatted(Formatting.GRAY)).append(channel);
+        MutableText channel = Text.literal(Discordmc.config.getCurrentChat() + "   ").formatted(Formatting.WHITE);
+        MutableText guild = Text.literal(Discordmc.config.getCurrentGuild()).formatted(Formatting.WHITE);
 
-		MutableText text = Text.literal("").formatted(Formatting.GRAY).append(chatDestination);
+        MutableText chatDestination = Text.literal("Chatting to: ").formatted(Formatting.GRAY).append(guild).append(Text.literal(", in #").formatted(Formatting.GRAY)).append(channel);
 
-		if (!Discordmc.discordChatEnabled) {
-			text = Text.literal("disabled").formatted(Formatting.DARK_GRAY);
-		}
+        MutableText text = Text.literal("").formatted(Formatting.GRAY).append(chatDestination);
 
-		MultilineTextWidget multilineTextWidget = getTextWidget(text, mc);
-		addDrawableChild(multilineTextWidget);
-	}
+        if (!Discordmc.config.isDiscordChatEnabled()) {
+            text = Text.literal("disabled").formatted(Formatting.DARK_GRAY);
+        }
 
-	@Unique
-	@NotNull
-	private MultilineTextWidget getTextWidget(MutableText text, MinecraftClient mc) {
-		MultilineTextWidget multilineTextWidget = new MultilineTextWidget(text, this.textRenderer);
-		multilineTextWidget.setPosition(mc.getWindow().getScaledWidth() - multilineTextWidget.getWidth() - 2, mc.getWindow().getScaledHeight() - 25);
-		return multilineTextWidget;
-	}
+        MultilineTextWidget multilineTextWidget = getTextWidget(text, mc);
+        addDrawableChild(multilineTextWidget);
+    }
+
+    @Unique
+    @NotNull
+    private MultilineTextWidget getTextWidget(MutableText text, MinecraftClient mc) {
+        MultilineTextWidget multilineTextWidget = new MultilineTextWidget(text, this.textRenderer);
+        multilineTextWidget.setPosition(mc.getWindow().getScaledWidth() - multilineTextWidget.getWidth() - 2, mc.getWindow().getScaledHeight() - 25);
+        return multilineTextWidget;
+    }
 }
